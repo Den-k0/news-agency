64847af22a0fca70a871e311357497d8cf51573d
Den_k0 <denys.kotovskiy@gmail.com>
2024-12-02 00:14:53 +0200

    implement Topic & Newspaper models

--- newspaper/models.py
+++ newspaper/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import Redactor
+
+
+class Topic(models.Model):
+    name = models.CharField()
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    publishers = models.ManyToManyField(Redactor, related_name="newspapers")
