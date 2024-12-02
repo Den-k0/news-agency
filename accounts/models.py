5d98e1983ee9569890c026b76ae7e3522f8cd646
Den_k0 <denys.kotovskiy@gmail.com>
2024-12-02 23:09:36 +0200

    Added __str__ representation to models

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,6 @@ from django.db import models
 
 class Redactor(AbstractUser):
     years_of_experience = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
--- newspaper/models.py
+++ newspaper/models.py
@@ -6,9 +6,15 @@ from accounts.models import Redactor
 class Topic(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 class Newspaper(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     published_date = models.DateTimeField()
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     publishers = models.ManyToManyField(Redactor, related_name="newspapers")
+
+    def __str__(self):
+        return self.title
