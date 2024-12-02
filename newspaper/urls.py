c7105ff74ff0cb7535a47fe0f1037ce2490fe8b6
Den_k0 <denys.kotovskiy@gmail.com>
2024-12-03 01:23:28 +0200

    Implement CRUD for Topic model (urls, views, new_templates, sidebar link)

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from newspaper.views import index
-
+from newspaper.views import (
+    index,
+    TopicListView,
+    TopicCreateView,
+    TopicUpdateView,
+    TopicDeleteView
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("topic/", TopicListView.as_view(), name="topic-list"),
+    path("topic/create", TopicCreateView.as_view(), name="topic-create"),
+    path("topic/<int:pk>/update", TopicUpdateView.as_view(), name="topic-update"),
+    path("topic/<int:pk>/delete", TopicDeleteView.as_view(), name="topic-delete"),
 ]
 
 app_name = "newspaper"
--- newspaper/views.py
+++ newspaper/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
 from accounts.models import Redactor
-from newspaper.models import Newspaper
+from newspaper.models import Newspaper, Topic
 
 
 def index(request):
@@ -12,3 +14,29 @@ def index(request):
         "number_of_news": number_of_news,
     }
     return render(request, "newspaper/index.html", context=context)
+
+
+# Topic
+class TopicListView(generic.ListView):
+    model = Topic
+    template_name = "newspaper/topic_list.html"
+
+
+class TopicCreateView(generic.CreateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("newspaper:topic-list")
+    template_name = "newspaper/topic_form.html"
+
+
+class TopicUpdateView(generic.UpdateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("newspaper:topic-list")
+    template_name = "newspaper/topic_form.html"
+
+
+class TopicDeleteView(generic.DeleteView):
+    model = Topic
+    success_url = reverse_lazy("newspaper:topic-list")
+    template_name = "newspaper/topic_confirm_delete.html"
