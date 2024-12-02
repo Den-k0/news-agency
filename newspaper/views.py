d08ed15c96cf3b4ab0ca665fa57a1c410956186e
Den_k0 <denys.kotovskiy@gmail.com>
2024-12-02 23:24:06 +0200

    Added context to index view & display context info in template

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from accounts.models import Redactor
+from newspaper.models import Newspaper
+
 
 def index(request):
-    return render(request, "newspaper/index.html")
+    number_of_redactors = Redactor.objects.count()
+    number_of_news = Newspaper.objects.count()
+    context = {
+        "number_of_redactors": number_of_redactors,
+        "number_of_news": number_of_news,
+    }
+    return render(request, "newspaper/index.html", context=context)
