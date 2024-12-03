9dd0e442e9045b6333aa56ecf3a87f6a18ea642f
Den_k0 <denys.kotovskiy@gmail.com>
2024-12-03 15:41:42 +0200

    Create NewspaaperDetailView

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views import generic, View
 
+from accounts.forms import RedactorCreationForm, RedactorUpdateForm
 from accounts.models import Redactor
 from newspaper.models import Newspaper, Topic
 
@@ -17,52 +19,56 @@ def index(request):
 
 
 # Topic
-class TopicListView(generic.ListView):
+class TopicListView(LoginRequiredMixin, generic.ListView):
     model = Topic
     template_name = "newspaper/topic_list.html"
 
 
-class TopicCreateView(generic.CreateView):
+class TopicCreateView(LoginRequiredMixin, generic.CreateView):
     model = Topic
     fields = "__all__"
     success_url = reverse_lazy("newspaper:topic-list")
     template_name = "newspaper/topic_form.html"
 
 
-class TopicUpdateView(generic.UpdateView):
+class TopicUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Topic
     fields = "__all__"
     success_url = reverse_lazy("newspaper:topic-list")
     template_name = "newspaper/topic_form.html"
 
 
-class TopicDeleteView(generic.DeleteView):
+class TopicDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Topic
     success_url = reverse_lazy("newspaper:topic-list")
     template_name = "newspaper/topic_confirm_delete.html"
 
 
 # Newspaper
-class NewspaperListView(generic.ListView):
+class NewspaperListView(LoginRequiredMixin, generic.ListView):
     model = Newspaper
     template_name = "newspaper/newspaper_list.html"
 
 
-class NewspaperCreateView(generic.CreateView):
+class NewspaperCreateView(LoginRequiredMixin, generic.CreateView):
     model = Newspaper
     fields = "__all__"
     success_url = reverse_lazy("newspaper:newspaper-list")
     template_name = "newspaper/newspaper_form.html"
 
 
-class NewspaperUpdateView(generic.UpdateView):
+class NewspaperUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Newspaper
     fields = "__all__"
     success_url = reverse_lazy("newspaper:newspaper-list")
     template_name = "newspaper/newspaper_form.html"
 
 
-class NewspaperDeleteView(generic.DeleteView):
+class NewspaperDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Newspaper
     success_url = reverse_lazy("newspaper:newspaper-list")
     template_name = "newspaper/newspaper_confirm_delete.html"
+
+
+class NewspaperDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Newspaper
