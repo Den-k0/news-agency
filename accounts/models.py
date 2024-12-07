b3d533f0ed8345af4f09911f2d0f5955d4f9a319
Den_k0 <denys.kotovskiy@gmail.com>
2024-12-08 00:05:04 +0200

    Implement get_absolute_url and improve interaction with detail view of redactor

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class Redactor(AbstractUser):
     years_of_experience = models.IntegerField(default=0)
+
+    def get_absolute_url(self):
+        return reverse("newspaper:redactor-detail", kwargs={"pk": self.pk})
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
